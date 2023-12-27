8a14e6207842e3ec80e6047cecc35707e099af58
Kashyap5551 <kash5551@my.yorku.ca>
2023-12-27 10:58:36 -0500

    cleaning up files

--- utils/uploadthing.ts
+++ utils/uploadthing.ts
@@ -1,4 +1,3 @@
 import { UTApi } from "uploadthing/server";
-import "dotenv/config";
-require("dotenv").config();
-export const utapi = new UTApi({ apiKey: process.env.UPLOADTHING_SECRET });
+
+export const utapi = new UTApi();
